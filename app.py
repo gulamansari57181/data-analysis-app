09585addf1834c52136b432f11d322d7cd76375a
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2025-04-17 19:17:27 +0530

    updated requiremets

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import numpy as np
 import matplotlib as plt
 import seaborn as sns
 import plotly.express as px
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import io
 import time
 
